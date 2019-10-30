5ba4965d9b3fa5f59226204b233ca1de712256ec
wellsimiao <wellsimiao@gmail.com>
2019-10-30 20:18:32 -0300

    FronEnd com Angular

--- src/app/contatos/contato.service.ts
+++ src/app/contatos/contato.service.ts
@@ -11,22 +11,21 @@ export class ContatoService {
   contatoUrl = 'http://localhost:8080/user/contato';
   constructor(private http: Http) { }
 
-  // tslint:disable-next-line: no-unused-expression
   getAllContato(): Observable[] > {
     return: this.http.get(this.allContatoUrl)
       .map(this.extractData)
       .catch(this.handleError);
-
-  }
-createContato(contato: Contato): Observable < number > {
-  let cpHeaders = new Headers({ 'Content-type': 'application/json' });
-  let option = new RequestOption({ headers: cpHeaders });
-  return this.http.post(this.contatoUrl, contato, options)
-    .map(sucess => sucess.status)
+  };
+// tslint:disable-next-line: no-unused-expression
+createContato(contato: Contato);: Observable < number > {
+  let Headers = new Headers({ 'Content-type': 'application/json' });
+  let Option = new RequestOption({ headers: cpHeaders });
+  return this.http.post(this.contatoUrl, contato, Option)
+    .map((sucess: { status: any; }) => sucess.status)
     .catch(this.handlError);
-}
-//GET: contato
-getContatoById(contatoId: string): Observable < Contato > {
+};
+// GET: contato
+getContatoById(contatoId: string); : Observable < Contato > {
   let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
   let cpParams = new URLSearchParams();
   cpParams.set('id', contatoId);
@@ -35,31 +34,31 @@ getContatoById(contatoId: string): Observable < Contato > {
     .map(this.extractData)
     // tslint:disable-next-line: indent
     .catch(this.handleError);
-}
+};
 // PUT: contato
-updateContato(contato: Contato): Observable < number > {
+updateContato(contato: Contato);: Observable < number > {
   let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
   let options = new RequestOptions({ headers: cpHeaders });
   return this.http.put(this.contatoUrl, contato, options)
     .map(success => success.status)
     .catch(this.handleError);
-}
-//DELETE: contato
-deleteContato(contatoId: string): Observable < number > {
+};
+
+// DELETE: contato
+deleteContato(contatoId: string);: Observable < number > {
   let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
   let cpParams = new URLSearchParams();
   cpParams.set('id', contatoId);
   let options = new RequestOptions({ headers: cpHeaders, params: cpParams });
   return this.http.delete(this.contatoUrl, options)
-    .map(success => success.status)
+    .map((success: { status: any; }) => success.status)
     .catch(this.handleError);
-}
-
-	private extractData(res: Response) {
-  let body = res.json();
+};
+	private extractData(res: Response); {
+  const body = res.json();
   return body;
 }
-  private handleError(error: Response | any) {
+  private handleError(error: Response | any); {
   console.error(error.message || error);
   return Observable.throw(error.status);
 }
